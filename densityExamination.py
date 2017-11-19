f139831ec68764e4aff45acfda015a55dd59cd09
Jedrzej Biedrzycki <jedrzejjanbiedrzycki@gmail.com>
2017-11-19 11:48:46 +0100

    Refactoring

--- densityExamination.py
+++ densityExamination.py
@@ -2,52 +2,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.ma.extras import average
 from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
-from func import prepare_samples, extract_coefficients, evaluate_coefficients
+import my_library
 
-number_of_samples = 900
+number_of_samples = 120
 mesh_step_size = .2
 partitioning = 5
+number_of_classifiers = 3
 
+clfs = my_library.initialize_classifiers(number_of_classifiers, LinearSVC())
 X, y = make_classification(n_features=2, n_redundant=0, n_informative=2, n_clusters_per_class=1,
                            n_samples=number_of_samples, class_sep=.7, hypercube=False, flip_y=0, random_state=4)
 X = StandardScaler().fit_transform(X)
-clf1, clf2, clf3 = LinearSVC(), LinearSVC(), LinearSVC()
-clfs = [clf1, clf2, clf3]
-number_of_classifiers = len(clfs)
-X1, Xrest, y1, yrest = train_test_split(X, y, train_size=int(number_of_samples / number_of_classifiers),
-                                        test_size=int(number_of_samples * 2 / number_of_classifiers))
-X2, X3, y2, y3 = train_test_split(Xrest, yrest, train_size=int(number_of_samples / number_of_classifiers),
-                                  test_size=int(number_of_samples / number_of_classifiers))
-X_whole, y_whole = [X1, X2, X3], [y1, y2, y3]
-scores, coefficients = [], []
-X_train, X_test, X_test_whole, y_train, y_test, y_test_whole = prepare_samples(X_whole, y_whole, number_of_samples,
-                                                                               number_of_classifiers)
+X_unsplitted, y_unsplitted, X_final_test, y_final_test = my_library.divide_samples(X, y, number_of_classifiers)
 
-i = 1
-x_min, x_max, y_min, y_max = X[:, 0].min(), X[:, 0].max(), X[:, 1].min(), X[:, 1].max()
+scores, coefficients = [], []
+Xs_train, Xs_test, Xs_test_whole, ys_train, ys_test, ys_test_whole = \
+    my_library.prepare_samples(X_unsplitted, y_unsplitted, 2 / 3)
+
+x_min, x_max, y_min, y_max = my_library.get_samples_limits(X)
 x_min_plot, x_max_plot, y_min_plot, y_max_plot = x_min - .5, x_max + .5, y_min - .5, y_max + .5
 xx, yy = np.meshgrid(np.arange(x_min_plot, x_max_plot, mesh_step_size), np.arange(y_min_plot, y_max_plot, mesh_step_size))
 
-for clf, X_tr, X_te, y_tr, y_te in zip(clfs, X_train, X_test, y_train, y_test):
+i = 1
+for clf, X_train, X_test, y_train, y_test in zip(clfs, Xs_train, Xs_test, ys_train, ys_test):
     score = []
-    clf.fit(X_tr, y_tr)
+    clf.fit(X_train, y_train)
     for j in range(partitioning):
-        x_samp_min, x_samp_max = x_min + j * (x_max - x_min) / partitioning, x_min + (j + 1) * (x_max - x_min) / partitioning
-        X_temp = [row for row in X_te if x_samp_min < row[0] < x_samp_max]
-        y_temp = [y_te[k] for k in range(len(y_te)) if x_samp_min < X_te[k][0] < x_samp_max]
-        if (len(X_temp) > 0):
-            score.append(clf.score(X_temp, y_temp))
+        X_part, y_part = my_library.prepare_samples_for_subspace(X_test, y_test, X, j, partitioning)
+        if (len(X_part) > 0):
+            score.append(clf.score(X_part, y_part))
         else:
             score.append(0)
+
     scores.append(score)
-    ax = plt.subplot(1, len(clfs) + 1, i)
-    ax.scatter(X_tr[:,0], X_tr[:,1], c=y_tr)
-    a, b = extract_coefficients(clf)
+    a, b = my_library.extract_coefficients(clf)
     coefficients.append([a, b])
+
+    ax = plt.subplot(1, number_of_classifiers + 1, i)
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train)
     x = np.linspace(x_min_plot, x_max_plot)
     y = a * x + b
     ax.plot(x, y)
@@ -55,21 +50,23 @@ for clf, X_tr, X_te, y_tr, y_te in zip(clfs, X_train, X_test, y_train, y_test):
     ax.set_ylim(yy.min(), yy.max())
     i += 1
 ax = plt.subplot(1, len(clfs) + 1, i)
-score=[]
+
+score, flip_index = [], 0
 for j in range(partitioning):
-    x_samp_min, x_samp_max = x_min + j * (x_max - x_min) / partitioning, x_min + (j + 1) * (x_max - x_min) / partitioning
-    x = np.linspace(x_samp_min, x_samp_max)
-    a, b = evaluate_coefficients(coefficients, number_of_classifiers, scores, j)
+    x_subspace_min, x_subspace_max = my_library.get_subspace_limits(X, j, partitioning)
+    x = np.linspace(x_subspace_min, x_subspace_max)
+    my_library.evaluate_coefficients_from_two_best(coefficients, number_of_classifiers, scores, j)
+    a, b = my_library.evaluate_coefficients(coefficients, number_of_classifiers, scores, j)
     y = a * x + b
     ax.plot(x, y)
-    X_temp = [row for row in X_test_whole if x_samp_min < row[0] < x_samp_max]
-    y_temp = [y_test_whole[k] for k in range(len(X_test_whole)) if x_samp_min < X_test_whole[k][0] < x_samp_max]
-    if (len(X_temp) > 0):
+    X_part = [row for row in Xs_test_whole if x_subspace_min < row[0] < x_subspace_max]
+    y_part = [ys_test_whole[k] for k in range(len(Xs_test_whole)) if x_subspace_min < Xs_test_whole[k][0] < x_subspace_max]
+    if (len(X_part) > 0):
         propperly_classified = 0
         all_classified = 0
         flip_index = 0
-        for k in range(len(X_temp)):
-            if ((a * X_temp[k][0] + b > X_temp[k][1]) ^ (y_temp[k] == 1)):
+        for k in range(len(X_part)):
+            if ((a * X_part[k][0] + b > X_part[k][1]) ^ (y_part[k] == 1)):
                 propperly_classified += 1
             all_classified += 1
         score.append(propperly_classified / all_classified)
@@ -86,8 +83,11 @@ scores.append(score)
 ax.set_xlim(xx.min(), xx.max())
 ax.set_ylim(yy.min(), yy.max())
 
+i = 1
 for row in scores:
+    print('Classifier ' + str(i))
     print(row)
+    i+=1
 for row in scores:
     print(average(row))
 plt.show()
---
+++ my_library.py
@@ -0,0 +1,77 @@
+from sklearn.model_selection import train_test_split
+
+
+def divide_samples(X, y, number_of_classifiers):
+    number_of_samples = len(X)
+    X_whole, y_whole, X_rest, y_rest = [], [], [], []
+    X_final_test, X_rest, y_final_test, y_rest = \
+        train_test_split(X, y, train_size=int(number_of_samples / (number_of_classifiers + 1)))
+    for i in range(number_of_classifiers - 1):
+        X_part, X_rest, y_part, y_rest = \
+            train_test_split(X_rest, y_rest, train_size=int(number_of_samples / (number_of_classifiers + 1)))
+        X_whole.append(X_part)
+        y_whole.append(y_part)
+    X_whole.append(X_rest)
+    y_whole.append(y_rest)
+    return X_whole, y_whole, X_final_test, y_final_test
+
+
+def prepare_samples(X_unsplitted, y_unsplitted, quotient):
+    X_train, X_test, y_train, y_test, X_test_whole, y_test_whole = [], [], [], [], [], []
+    for X_one, y_one in zip(X_unsplitted, y_unsplitted):
+        X_train_part, X_test_part, y_train_part, y_test_part = \
+            train_test_split(X_one, y_one, train_size=int(len(X_one) * quotient))
+        X_train.append(X_train_part)
+        X_test.append(X_test_part)
+        X_test_whole[len(X_test_whole):] = X_test_part[:]
+        y_train.append(y_train_part)
+        y_test.append(y_test_part)
+        y_test_whole[len(y_test_whole):] = y_test_part[:]
+
+    return X_train, X_test, X_test_whole, y_train, y_test, y_test_whole
+
+
+def extract_coefficients(clf):
+    a, b = - clf.coef_[0][0] / clf.coef_[0][1], - clf.intercept_[0] / clf.coef_[0][1]
+    return a, b
+
+
+def evaluate_coefficients(coefficients, number_of_classifiers, scores, j):
+    a, b, sumOfScores = 0, 0, 0
+    for k in range(number_of_classifiers):
+        a += coefficients[k][0] * scores[k][j]
+        b += coefficients[k][1] * scores[k][j]
+        sumOfScores += scores[k][j]
+    return a / sumOfScores, b / sumOfScores
+
+def evaluate_coefficients_from_two_best(coefficients, number_of_classifiers, scores, j):
+    a, b, sumOfScores, params = 0, 0, 0, []
+    for i in range(number_of_classifiers):
+        params.append([scores[i][j], coefficients[i]])
+    params.sort()
+    print(params)
+
+
+def initialize_classifiers(number_of_classifiers, classifier):
+    clfs = []
+    for i in range(number_of_classifiers):
+        clfs.append(classifier)
+    return clfs
+
+
+def get_samples_limits(X):
+    return X[:, 0].min(), X[:, 0].max(), X[:, 1].min(), X[:, 1].max()
+
+
+def prepare_samples_for_subspace(X_test, y_test, X, j, partitioning):
+    x_samp_max, x_samp_min = get_subspace_limits(X, j, partitioning)
+    X_part = [row for row in X_test if x_samp_min < row[0] < x_samp_max]
+    y_part = [y_test[k] for k in range(len(y_test)) if x_samp_min < X_test[k][0] < x_samp_max]
+    return X_part, y_part
+
+
+def get_subspace_limits(X, j, partitioning):
+    x_subspace_min, x_subspace_max = \
+        X[:, 0].min() + j * (X[:, 0].max() - X[:, 0].min()) / partitioning, \
+        X[:,0].min() + (j + 1) * (X[:, 0].max() - X[:, 0].min()) / partitioning
+    return x_subspace_max, x_subspace_min
