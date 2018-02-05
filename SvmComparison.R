bfb7c7107dd27769c3505a88bba9af0d0a96cb88
Jędrzej Biedrzycki <jedrzejjanbiedrzycki@gmail.com>
2018-02-05 22:40:45 +0100

    Prepare scores of R classifiers

--- SvmComparison.R
+++ SvmComparison.R
@@ -1,16 +1,19 @@
 library("readxl")
 library("e1071")
 
-setwd("D:\\Workspace\\R")
+# setwd("D:\\Workspace\\R")
 
+print("Reading data")
 data <- read_excel("datasets.xlsx", sheet = 13, col_names = FALSE)
 
+print("Preparing data")
 X = cbind(data[1], data[2])
 X = sapply(X, as.numeric)
 y = sapply(data[length(data)], as.numeric)
 y = as.factor(y)
 train.data = data.frame(X, y)
 
+print("Training model")
 # model <- svm(X, y, kernel = "linear", type = "C-classification")
 model <- svm(y ~ ., data = train.data, kernel = "linear")
 print(names(model))
@@ -22,5 +25,16 @@ print(names(train.data))
 # my.svm <- svm(~ ., kernel = "linear", data = train.data, type = "C-classification")
 # print(summary(my.svm))
 # plot(my.svm, train.data, X__2 ~ X__1)
-plot(model, train.data, X__2 ~ X__1, xlim = c(-5, 0), ylim = c(0, 5), fill = TRUE)
+
+print("Plotting model")
+# plot(model, train.data, X__2 ~ X__1, xlim = c(-5, 0), ylim = c(0, 5), fill = TRUE)
+plot(model, train.data, X__2 ~ X__1)
 print(summary(model))
+
+print("Checking accuracy")
+pred <- fitted(model)
+print(table(pred, y))
+
+print("Checking decission values")
+pred <- predict(model, X, decision.values = TRUE)
+# print(attr(pred, "decision.values"))
