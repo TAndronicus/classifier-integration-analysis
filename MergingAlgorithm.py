d098dc3201375f8eac81e467d334d95db5f7ddce
Jędrzej Biedrzycki <jedrzejjanbiedrzycki@gmail.com>
2018-02-06 19:15:53 +0100

    Add column changing

--- MergingAlgorithm.py
+++ MergingAlgorithm.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import MyLibrary
 
 type_of_classifier = MyLibrary.ClfType.LINEAR
-are_samples_generated = True
+are_samples_generated = False
 number_of_samples_if_generated = 10000
 number_of_dataset_if_not_generated = 1
-switch_columns_while_loading = False
+switch_columns_while_loading = True
 plot_mesh_step_size = .2
 number_of_space_parts = 5
 number_of_classifiers = 3
 number_of_best_classifiers = number_of_classifiers - 1
 draw_color_plot = False
 write_computed_scores = True
-show_plots = False
+show_plots = True
 
 clfs = MyLibrary.initialize_classifiers(number_of_classifiers, type_of_classifier)
 
