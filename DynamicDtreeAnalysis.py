038def4cb255901a4f70863a7cf51d5cc0b2eb69
TAndronicus <jb0209jj1994i>
2020-07-13 15:56:45 +0200

    Change logic for dynamic tree

--- DynamicDtreeAnalysis.py
+++ DynamicDtreeAnalysis.py
@@ -1,54 +1,55 @@
 import os
 
-from DtdDisplacementRes import DtdDisplacementRes
+from DynamicDtreeRes import DynamicDtreeRes
 from MathUtils import round_to_str
 from nonparametric_tests import friedman_test, bonferroni_dunn_test
 
 import numpy as np
 
 filenames = ['bio', 'bup', 'cry', 'dba', 'hab', 'ion', 'met', 'pop', 'sei', 'wdb', 'wis']
-references = ['mv', 'rf']
-n_clfs = [3, 5, 7, 9]
+references = ['mv', 'rf', 'i']
+n_clfs = [3]  # [3, 5, 7, 9]
 n_meas = 4
-
-even_indices = np.arange(0, n_displacements ** n_feas) * n_meas
-odd_indices = np.arange(0, n_displacements ** n_feas) * n_meas + 1
+metrics = ['euclidean']
+mappings = ['???']
 
 
 def read(n_clf, metric, mapping):
-    name_pattern = 'dtd-displacement/{}_{}_[' + '_'.join([str(el) for el in n_divs]) + ']_{}'
-    res_filename = name_pattern.format(n_clf, n_fea, n_displacements)
+    name_pattern = 'dynamic-dtree/{}_{}_{}'
+    res_filename = name_pattern.format(n_clf, metric, mapping)
     absolute_path = os.path.join(os.path.dirname(__file__), res_filename)
     objects = []
     with(open(absolute_path)) as file:
-        counter = 0
-        for line in file.readlines():
-            if counter != 0:  # header
-                values = line.split(',')
-                np.take(np.array(values[6:n_meas * (n_displacements ** n_feas) + 6], dtype=float),
-                        even_indices)  # acc div20
-                obj = DynamicDtreeRes(
-                    float(values[0]), float(values[1]), float(values[2]), float(values[3]),  # mv
-                    float(values[4]), float(values[5]), float(values[6]), float(values[7]),  # rf
-                    float(values[8]), float(values[9]), float(values[10]), float(values[11]),  # i
-                    n_clf, metric, mapping, filenames[counter - 1])
-                objects.append(obj)
-            counter += 1
+        for counter, line in enumerate(file.readlines()):
+            values = line.split(',')
+            obj = DynamicDtreeRes(
+                float(values[0]), float(values[1]), float(values[2]), float(values[3]),  # mv
+                float(values[4]), float(values[5]), float(values[6]), float(values[7]),  # rf
+                float(values[8]), float(values[9]), float(values[10]), float(values[11]),  # i
+                n_clf, metric, mapping, filenames[counter - 1])
+            objects.append(obj)
     return objects
+
+
+def read_all(n_clf):
+    for metric in metrics:
+        for mapping in mappings:
+            yield read(n_clf, metric, mapping)
 
 
 def compose_dict(values, aggregate, even=True, vol=True):
     bias = 6 if vol else (8 + len(n_divs) * n_meas * (n_displacements ** n_feas))
     return {
         n_div: aggregate(np.take(np.array(values[i * n_meas * (n_displacements ** n_feas) + bias:(i + 1) * n_meas * (
-                    n_displacements ** n_feas) + bias], dtype=float), even_indices if even else odd_indices))
+                n_displacements ** n_feas) + bias], dtype=float), even_indices if even else odd_indices))
         for i, n_div in enumerate(n_divs)}
 
 
-def map_dtrex(objects, attr, method):
+def map_dtrex(objects, attr, n_clf):
     res_out = []
+    all_objs = read_all(n_clf)
     for reference in references:
-        res_out.append([getattr(obj, reference + '_' + attr) for obj in objects])
+        res_out.append([getattr(obj, reference + '_' + attr) for obj in all_objs])
     for div in n_divs:
         res_out.append([getattr(obj, 'i_' + method + '_' + attr)[div] for obj in objects])
     return res_out
@@ -85,18 +86,21 @@ def double_script_psi(subscript: str, superscript: str):
 
 def print_results(file_to_write=None):
     for meas in ['acc', 'mcc', 'f1', 'aur']:
-        for metric in ['euclidean']:
-            for mapping in ['???']:
+        for mapping in ['???']:
+            for metric in ['euclidean']:
                 for n_clf in n_clfs:
-                    custom_print('\nn_fea: ' + str(n_feas) + ', meas: ' + meas + ', n_clf: ' + str(
-                        n_clf) + ', mapping: ' + mapping + '\n', file_to_write)
+                    custom_print('\nn_clf: ' + str(n_clf) +
+                                 ', metric: ' + metric +
+                                 ', mapping: ' + mapping +
+                                 ', meas: ' + meas + '\n',
+                                 file_to_write)
 
                     for filename in filenames:
                         custom_print(',' + filename, file_to_write)
                     custom_print(',rank\n', file_to_write)
 
-                    objs = read(n_clf, n_feas)
-                    values = map_dtrex(objs, meas, mapping)
+                    objs = read(n_clf, metric, mapping)  # metric & mapping wspólne
+                    values = map_dtrex(objs, meas, n_clf)
                     iman_davenport, p_value, rankings_avg, rankings_cmp = friedman_test(values)
 
                     counter = 0
@@ -117,12 +121,12 @@ def print_results(file_to_write=None):
                         custom_print(round_to_str(rankings_cmp[counter], 2) + '\n', file_to_write)
                         counter = counter + 1
 
-                ## post-hoc
+                    ## post-hoc
                     rankings = create_rank_dict(rankings_cmp)
                     comparisonsH, z, pH, adj_p = bonferroni_dunn_test(rankings, '0')
                     pH = [x for _, x in sorted(zip(comparisonsH, pH))]
                     custom_print('p-values: ' + str(pH) + '\n', file_to_write)
 
 
-with open('reports/1-displacement.csv', 'w') as f:
+with open('reports/3-dynamic-dtree.csv', 'w') as f:
     print_results(f)
