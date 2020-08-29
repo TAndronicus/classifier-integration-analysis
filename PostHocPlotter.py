ab96808f6a2f66d2bc6019bf62d3954acae5b953
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-08-29 13:27:26 +0200

    Rename PostHocPlotter.py

--- PostHocPlotter.py
+++ PostHocPlotter.py
@@ -1,20 +1,18 @@
-from Orange.evaluation import graph_ranks
 import matplotlib.pyplot as plt
+from Orange.evaluation import graph_ranks
 from matplotlib import rc
 
-rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 12})
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'], 'size': 15})
 ## for Palatino and other serif fonts use:
 # rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex = True)
 
-critical_value = 1.66
+critical_value = 0.38
 algorithms = {
     # @formatter:off
-    '$\Psi_{mv}$':       5.19,
-    '$\Psi_{rf}$':  2.29,
-    '$\Psi_{20}$': 5.26,
-    '$\Psi_{40}$': 4.25,
-    '$\Psi_{60}$': 5.26
+    '$\Psi_{mv}$': 2,
+    '$\Psi_{rf}$': 2.14,
+    '$\Psi_{i}$': 1.61
     # @formatter:on
 }
 avranks = [*algorithms.values()]
