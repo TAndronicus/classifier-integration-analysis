af4168d96bbfb56f161779c236468c2e607aea07
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-11-28 21:50:32 +0100

    Fix script ordering

--- sql/0_cleanup.sql
+++ sql/0_cleanup.sql
@@ -1,5 +1,7 @@
-drop table files;
-drop table metrics;
-drop table mappings;
+drop table if exists dynamic_ring_raw;
+drop table if exists dynamic_ring;
 
-drop table dynamic_ring_raw;
+drop table if exists files;
+drop table if exists metrics;
+drop table if exists mappings;
+
