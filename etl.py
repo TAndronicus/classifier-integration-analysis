19c672dd99718e9aa894d22bb9565ae1e840319d
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-11-28 04:14:29 +0100

    Cleanup etl

--- etl.py
+++ etl.py
@@ -1,93 +1,9 @@
-import os
 from math import pow, sqrt
 
-import numpy as np
 from texttable import Texttable
-
-filenames = np.array([
-    "aa",
-    "ap",
-    "ba",
-    "bi",
-    "bu",
-    "c",
-    "d",
-    "e",
-    "h",
-    "io",
-    "ir",
-    "me",
-    "ma",
-    "po",
-    "ph",
-    "pi",
-    "ri",
-    "sb",
-    "se",
-    "tw",
-    "te",
-    "th",
-    "ti",
-    "wd",
-    "wi",
-    "wr",
-    "ww",
-    "y"])
-algorithms = ['MV', 'RF', 'wMV', 'I']
-mappings = ['vol', 'inv']
-divs = [20, 40, 60]
-measurements = ['acc', 'precision', 'recall', 'fscore', 'specificity', 'auc']
-displacements = [1, 5]
-clfs = [3, 5]
-has_header = True
-n_filenames = len(filenames)
-n_algorithms_independent = 2
-n_algorithms_mapping_dep = 1
-n_algorithms_mapping_div_dep = 1
-n_algorithms = n_algorithms_independent + n_algorithms_mapping_dep + n_algorithms_mapping_div_dep
-n_mappings = len(mappings)
-n_divs = len(divs)
-n_measurements = len(measurements)
-n_displacements = len(displacements)
-n_clfs = len(clfs)
-
-num_features = 2
-ANY = 0
 
 
 class Etl:
-
-    def chunker(self, seq, size):
-        return (seq[pos:pos + size] for pos in range(0, len(seq), size))
-
-    def read_cube(self):
-        res = np.zeros((n_filenames, n_algorithms, n_mappings, n_divs, n_measurements, n_displacements, n_clfs))
-        for (o, clf) in enumerate(clfs):
-            for (n, displacement) in enumerate(displacements):
-                name_pattern = 'dtd-displacement/{}_{}_[' + '_'.join([str(el) for el in divs]) + ']_{}'
-                res_filename = name_pattern.format(clf, num_features, displacement)
-                absolute_path = os.path.join(os.path.dirname(__file__), res_filename)
-                with(open(absolute_path)) as file:
-                    for (line_num, line) in enumerate(file.readlines()):
-                        if has_header and line_num == 0: continue
-                        file_index = line_num - [0, 1][has_header]
-                        values = line.split(',')
-                        assert len(values) == (
-                                n_algorithms_independent + n_algorithms_mapping_dep * n_mappings + n_algorithms_mapping_div_dep * n_mappings * n_divs) * n_measurements
-                        for (algorithm_variation_index, algorithm_values) in enumerate(self.chunker(values, n_measurements)):
-                            print(f"filename nr: {file_index}, displacements: {displacements[n]}, clfs: {clfs[o]}, values: {algorithm_values}")
-                            if file_index == 27:
-                                self.print_conf_matrix(self.calculate_conf_matrix(float(algorithm_values[0]), float(algorithm_values[2]), float(algorithm_values[3]), 100))
-                            if algorithm_variation_index < n_algorithms_independent:
-                                res[file_index, algorithm_variation_index, :, :, :, n, o] = algorithm_values
-                            elif algorithm_variation_index < n_algorithms_independent + n_algorithms_mapping_dep * n_mappings:
-                                algorithm_index = n_algorithms_independent + int((algorithm_variation_index - n_algorithms_independent) / n_mappings)
-                                res[file_index, algorithm_index, algorithm_variation_index - n_algorithms_independent, :, :, n, o] = algorithm_values
-                            else:
-                                rest = algorithm_variation_index - n_algorithms_independent - n_algorithms_mapping_dep * n_mappings
-                                algorithm_index = n_algorithms_independent + n_algorithms_mapping_dep + int(rest / (n_mappings * n_divs))
-                                res[file_index, algorithm_index, rest % n_mappings, int(rest / n_mappings), :, n, o] = algorithm_values
-        return res
 
     def calculate_conf_matrix(self, acc, precision, recall, size):
         tp = size * (1 - acc) / (1 / precision + 1 / recall - 2)
