35b75977578e6ede8bfc6f783ac9cceab2bed645
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-11-29 11:36:13 +0100

    Add stats table

--- sql/0_cleanup.sql
+++ sql/0_cleanup.sql
@@ -1,7 +1,10 @@
 drop table if exists dynamic_ring_raw;
 drop table if exists dynamic_ring;
+drop table if exists dynamic_ring_stats;
 
 drop table if exists files;
 drop table if exists metrics;
 drop table if exists mappings;
 
+drop sequence files_seq;
+drop sequence mes_seq;
